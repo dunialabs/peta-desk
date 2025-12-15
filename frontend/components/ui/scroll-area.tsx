4a544987451c7b3e54ccb5fb61f7588c62167b8d
Miles-ZF <yufei@kompas.ai>
2025-12-16 00:58:13 +0800

    scroll area Adapt UI for dark model

--- frontend/components/ui/scroll-area.tsx
+++ frontend/components/ui/scroll-area.tsx
@@ -40,7 +40,7 @@ const ScrollBar = React.forwardRef<
     )}
     {...props}
   >
-    <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 rounded-full bg-border dark:bg-gray-700" />
+    <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 rounded-full bg-border dark:bg-gray-600 hover:dark:bg-gray-500" />
   </ScrollAreaPrimitive.ScrollAreaScrollbar>
 ))
 ScrollBar.displayName = ScrollAreaPrimitive.ScrollAreaScrollbar.displayName
